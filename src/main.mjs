38ec3e65141dc153a08c1dc0692e39dbbe7454dc
LonelyBush <lonely.bush68@gmail.com>
2025-05-05 23:51:00 +0300

    feat: add cat func

--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,9 @@
 
-import { access, readdir } from 'fs/promises';
+import { access, readdir, stat, } from 'fs/promises';
 import {createInterface} from 'readline';
 import os from 'os';
 import path from 'path';
+import { createReadStream } from 'fs';
 
 let currentDir = os.homedir();
 
@@ -21,7 +22,7 @@ const rl = createInterface({
 })
 
 rl.write(`Welcome to the File Manager, ${userName}!` + '\n');
-
+process.stdout.write(`\nYou are currently in ${currentDir}\n`);
 
 rl.on('SIGINT', () => {
     console.log('\n'+`Thank you for using File Manager, ${userName}, goodbye!`)
@@ -40,12 +41,18 @@ const moveUp = () => {
 
 const moveDown = async (sourcePath) => {
     const newPath = path.join(currentDir, sourcePath)
-    console.log(newPath);
+
     try {
         await access(path.normalize(newPath));
-        currentDir = path.normalize(newPath);
-      } catch {
-        rl.write('Invalid path try again !!' + '\n');
+        const dirent = await stat(path.normalize(newPath))
+
+        if(dirent.isDirectory()){
+            currentDir = path.normalize(newPath);
+        } else {
+            throw new Error('You`ve tried to navigate on file !')
+        }
+      } catch(err) {
+        rl.write(`\n${err}\n`);
       }
 
 }
@@ -56,9 +63,33 @@ const handleList = async () => {
     .sort((a, b) => a.Type.localeCompare(b.Type) || a.Name.localeCompare(b.Name)));
 }
 
-function main(question) {
+const handleReadFile = async (sourcePath) => {
+    const newPath = path.join(currentDir, sourcePath)
+    try {
+        await access(newPath);
+    } catch {
+        throw new Error(`ENOENT: no such file or directory, open '${newPath}'`);
+    }
 
-    rl.write(`You are currently in ${currentDir}`  + '\n')
+    return new Promise((resolve, reject) => {
+        const read = createReadStream(newPath, { encoding: 'utf-8' });
+
+        read.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+
+        read.on('end', () => {
+            process.stdout.write('\n');
+            resolve();
+        });
+
+        read.on('error', (error) => {
+            reject(error);
+        });
+    });
+}
+
+function main(question) {
 
     rl.question(question, async (answer) => {
         const [command, arg] = answer.split(' ');
@@ -69,12 +100,28 @@ function main(question) {
                 process.exit(0)
             case 'up':
                 moveUp(currentDir);
+                process.stdout.write(`\nYou are currently in ${currentDir}\n`);
                 break;
             case 'cd':
                 await moveDown(arg?? '');
+                process.stdout.write(`\nYou are currently in ${currentDir}\n`);
                 break;
             case 'ls':
                 await handleList()
+                process.stdout.write(`\nYou are currently in ${currentDir}\n`);
+                break;
+            case 'cat':
+                if (!arg) {
+                    console.error('\nFile name required\n');
+                    return main(question);
+                }
+                handleReadFile(arg?? '').catch((error) => {
+                    console.error(`\n${error.message}`);
+                })
+                .finally(() => {
+                    process.stdout.write(`\nYou are currently in ${currentDir}\n`);
+                    main(question);
+                });
                 break;
             default:
                 rl.write('Invalid input' + '\n')
